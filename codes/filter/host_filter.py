afd7cf6f41ba00bd224cda98a42fe86657a05e41
Lyra Liu <lyra11liu@gmail.com>
2025-11-14 09:14:51 +0000

    update.

--- codes/filter/host_filter.py
+++ codes/filter/host_filter.py
@@ -4,5 +4,20 @@
 """
 host_filter.py
 Author: lyra LIU
-
+Batch Bam.: removehuman seqs, extract unmapped reads after minimap2 alignment against human reference.
+Support parallel processing, multiple log, timing, & resource monitoring.
 """
+from __future__ import annotations
+import os, sys, re, time, json, shutil, argparse, subprocess, datetime
+from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+try:
+    import psutil
+except Exception:
+    psutil = None
+    
+def which(x: str) -> str:
+    p = shutil.which(x)
+    if not p:
+        raise FileExistsError(f"[ERR] Can't find dependence: {x}")
